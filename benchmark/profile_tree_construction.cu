eb1676f922df7b4e6d287534be653ffcaeb02c82
Marco Franzreb <franzrebmarco@gmail.com>
2024-12-15 09:38:22 +0100

    Improve tree profile

--- benchmark/profile_tree_construction.cu
+++ benchmark/profile_tree_construction.cu
@@ -7,20 +7,45 @@ int main(int argc, char** argv) {
   // size is first command line argument
   auto const data_size = std::stoul(argv[1]);
   auto const alphabet_size = std::stoul(argv[2]);
+  auto const use_min_alphabet = static_cast<bool>(std::stoi(argv[3]));
 
   if (alphabet_size < std::numeric_limits<uint8_t>::max()) {
-    auto [alphabet, data] = ecl::generateRandomAlphabetAndData<uint8_t>(
-        alphabet_size, data_size, true);
+    std::vector<uint8_t> alphabet;
+    std::vector<uint8_t> data;
+    if (use_min_alphabet) {
+      alphabet = std::vector<uint8_t>(alphabet_size);
+      std::iota(alphabet.begin(), alphabet.end(), 0);
+      data = ecl::generateRandomData<uint8_t>(alphabet, data_size);
+    } else {
+      std::tie(alphabet, data) = ecl::generateRandomAlphabetAndData<uint8_t>(
+          alphabet_size, data_size, true);
+    }
     ecl::WaveletTree<uint8_t> wt(data.data(), data_size, std::move(alphabet),
                                  0);
   } else if (alphabet_size < std::numeric_limits<uint16_t>::max()) {
-    auto [alphabet, data] = ecl::generateRandomAlphabetAndData<uint16_t>(
-        alphabet_size, data_size, true);
+    std::vector<uint16_t> alphabet;
+    std::vector<uint16_t> data;
+    if (use_min_alphabet) {
+      alphabet = std::vector<uint16_t>(alphabet_size);
+      std::iota(alphabet.begin(), alphabet.end(), 0);
+      data = ecl::generateRandomData<uint16_t>(alphabet, data_size);
+    } else {
+      std::tie(alphabet, data) = ecl::generateRandomAlphabetAndData<uint16_t>(
+          alphabet_size, data_size, true);
+    }
     ecl::WaveletTree<uint16_t> wt(data.data(), data_size, std::move(alphabet),
                                   0);
   } else {
-    auto [alphabet, data] = ecl::generateRandomAlphabetAndData<uint32_t>(
-        alphabet_size, data_size, true);
+    std::vector<uint32_t> alphabet;
+    std::vector<uint32_t> data;
+    if (use_min_alphabet) {
+      alphabet = std::vector<uint32_t>(alphabet_size);
+      std::iota(alphabet.begin(), alphabet.end(), 0);
+      data = ecl::generateRandomData<uint32_t>(alphabet, data_size);
+    } else {
+      std::tie(alphabet, data) = ecl::generateRandomAlphabetAndData<uint32_t>(
+          alphabet_size, data_size, true);
+    }
     ecl::WaveletTree<uint32_t> wt(data.data(), data_size, std::move(alphabet),
                                   0);
   }

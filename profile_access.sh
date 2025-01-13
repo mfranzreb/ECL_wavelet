c6429be72afc8a35604a7e4e910b721a7201d1d2
Marco Franzreb <franzrebmarco@gmail.com>
2025-01-13 17:54:47 +0100

    Improve access profile

--- profile_access.sh
+++ profile_access.sh
@@ -10,8 +10,8 @@ ncu --export "./access_1G_150_5000" --force-overwrite --kernel-id ::accessKernel
 
 ncu --export "./access_500M_20000_5000" --force-overwrite --kernel-id ::accessKernel: --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --import-source yes ./build/benchmark/ecl_access_profile 500000000 20000 5000
 
-nvprof --export-profile "./access_1G_4_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 4 5000
+nvprof --profile-from-start off --export-profile "./access_1G_4_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 4 5000
 
-nvprof --export-profile "./access_1G_150_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 150 5000
+nvprof --profile-from-start off --export-profile "./access_1G_150_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 150 5000
 
-nvprof --export-profile "./access_500M_20000_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 500000000 20000 5000
+nvprof --profile-from-start off --export-profile "./access_500M_20000_5000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 500000000 20000 5000

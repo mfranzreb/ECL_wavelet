70f3ac7d53c73bfc9b79a58a1097c40e7838723c
Marco Franzreb <franzrebmarco@gmail.com>
2025-01-14 12:39:07 +0100

    Add rank benchmark

--- benchmark/benchmark_rank.cu
+++ benchmark/benchmark_rank.cu
@@ -13,7 +13,22 @@
 namespace ecl {
 
 template <typename T>
-static void BM_Access(benchmark::State& state) {
+std::vector<RankSelectQuery<T>> generateRandomQueries(
+    size_t const data_size, size_t const num_queries,
+    std::vector<T> const& alphabet) {
+  std::vector<RankSelectQuery<T>> queries(num_queries);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<size_t> dis(0, data_size - 1);
+  std::uniform_int_distribution<T> dis2(0, alphabet.size() - 1);
+  std::generate(queries.begin(), queries.end(), [&]() {
+    return RankSelectQuery<T>(dis(gen), alphabet[dis2(gen)]);
+  });
+  return queries;
+}
+
+template <typename T>
+static void BM_Rank(benchmark::State& state) {
   auto const data_size = state.range(0);
   auto const alphabet_size = state.range(1);
   auto const num_queries = state.range(2);
@@ -26,7 +41,7 @@ static void BM_Access(benchmark::State& state) {
   state.counters["param.alphabet_size"] = alphabet_size;
   state.counters["param.num_queries"] = num_queries;
 
-  auto queries = generateRandomQueries(data_size, num_queries);
+  auto queries = generateRandomQueries<T>(data_size, num_queries, alphabet);
 
   auto wt = WaveletTree<T>(data.data(), data_size, std::move(alphabet), 0);
   // Memory usage
@@ -39,20 +54,22 @@ static void BM_Access(benchmark::State& state) {
     while (not can_start) {
       std::this_thread::yield();
     }
-    auto results = wt.access(queries);
+    auto results = wt.rank(queries);
     done = true;
     t.join();
     state.counters["memory_usage"] = max_memory_usage;
   }
 
   for (auto _ : state) {
-    auto results = wt.access(queries);
+    auto results = wt.rank(queries);
   }
 }
 
 template <nvbio::Alphabet AlphabetType>
-nvbio::PackedVector<nvbio::device_tag,
-                    nvbio::AlphabetTraits<AlphabetType>::SYMBOL_SIZE, true>
+std::pair<
+    nvbio::PackedVector<nvbio::device_tag,
+                        nvbio::AlphabetTraits<AlphabetType>::SYMBOL_SIZE, true>,
+    std::vector<uint8_t>>
 getNVbioArgs(size_t const data_size, uint8_t const alphabet_size = 0) {
   std::vector<uint8_t> alphabet;
   std::vector<uint8_t> data(data_size);
@@ -85,7 +102,7 @@ getNVbioArgs(size_t const data_size, uint8_t const alphabet_size = 0) {
   // copy it to the device
   nvbio::PackedVector<nvbio::device_tag, alphabet_bits, true> d_data(h_data);
 
-  return d_data;
+  return std::make_pair(d_data, alphabet);
 }
 
 static void BM_NVBIO(benchmark::State& state) {
@@ -100,54 +117,61 @@ static void BM_NVBIO(benchmark::State& state) {
   state.counters["param.data_size"] = data_size;
   state.counters["param.alphabet_size"] = alphabet_size;
 
-  auto queries = generateRandomQueries(data_size, num_queries);
-
   if (alphabet_size == 4) {
-    auto d_data = getNVbioArgs<nvbio::DNA>(data_size);
+    auto [d_data, alphabet] = getNVbioArgs<nvbio::DNA>(data_size);
     nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
     nvbio::setup(data_size, d_data.begin(), wt);
     auto const wt_view = nvbio::plain_view(
         (const nvbio::WaveletTreeStorage<nvbio::device_tag>&)wt);
+    auto queries =
+        generateRandomQueries<uint8_t>(data_size, num_queries, alphabet);
     for (auto _ : state) {
 #pragma omp parallel for
       for (auto const& query : queries) {
-        nvbio::text(wt_view, query);
+        nvbio::rank(wt_view, query.index_, query.symbol_);
       }
     }
   } else if (alphabet_size == 5) {
-    auto d_data = getNVbioArgs<nvbio::DNA_N>(data_size);
+    auto [d_data, alphabet] = getNVbioArgs<nvbio::DNA_N>(data_size);
     nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
     nvbio::setup(data_size, d_data.begin(), wt);
     auto const wt_view = nvbio::plain_view(
         (const nvbio::WaveletTreeStorage<nvbio::device_tag>&)wt);
+    auto queries =
+        generateRandomQueries<uint8_t>(data_size, num_queries, alphabet);
     for (auto _ : state) {
 #pragma omp parallel for
       for (auto const& query : queries) {
-        nvbio::text(wt_view, query);
+        nvbio::rank(wt_view, query.index_, query.symbol_);
       }
     }
   } else if (alphabet_size == 24) {
-    auto d_data = getNVbioArgs<nvbio::PROTEIN>(data_size);
+    auto [d_data, alphabet] = getNVbioArgs<nvbio::PROTEIN>(data_size);
     nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
     nvbio::setup(data_size, d_data.begin(), wt);
     auto const wt_view = nvbio::plain_view(
         (const nvbio::WaveletTreeStorage<nvbio::device_tag>&)wt);
+    auto queries =
+        generateRandomQueries<uint8_t>(data_size, num_queries, alphabet);
     for (auto _ : state) {
 #pragma omp parallel for
       for (auto const& query : queries) {
-        nvbio::text(wt_view, query);
+        nvbio::rank(wt_view, query.index_, query.symbol_);
       }
     }
   } else {
-    auto d_data = getNVbioArgs<nvbio::ASCII>(data_size, alphabet_size);
+    auto [d_data, alphabet] =
+        getNVbioArgs<nvbio::ASCII>(data_size, alphabet_size);
     nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
     nvbio::setup(data_size, d_data.begin(), wt);
     auto const wt_view = nvbio::plain_view(
         (const nvbio::WaveletTreeStorage<nvbio::device_tag>&)wt);
+    auto queries =
+        generateRandomQueries<uint8_t>(data_size, num_queries, alphabet);
     for (auto _ : state) {
 #pragma omp parallel for
       for (auto const& query : queries) {
-        nvbio::text(wt_view, query);
+        nvbio::rank(wt_view, query.index_, query.symbol_);
       }
     }
   }
@@ -163,11 +187,10 @@ static void BM_SDSL(benchmark::State& state) {
   state.counters["param.alphabet_size"] = alphabet_size;
   state.counters["param.num_queries"] = num_queries;
 
-  auto queries = generateRandomQueries(data_size, num_queries);
-
   auto [alphabet, data] =
       generateRandomAlphabetAndData<T>(alphabet_size, data_size, true);
 
+  auto queries = generateRandomQueries<T>(data_size, num_queries, alphabet);
   // write data to file
   std::ofstream data_file("data_file");
   for (auto const& d : data) {
@@ -182,20 +205,20 @@ static void BM_SDSL(benchmark::State& state) {
   for (auto _ : state) {
 #pragma omp parallel for
     for (size_t i = 0; i < num_queries; ++i) {
-      results_sdsl[i] = wt[i];
+      results_sdsl[i] = wt.rank(queries[i].index_, queries[i].symbol_);
     }
   }
 }
 
 // For initializing CUDA
-BENCHMARK(BM_Access<uint8_t>)
+BENCHMARK(BM_Rank<uint8_t>)
     ->Args({100, 4, 1})
     ->Iterations(10)
     ->Unit(benchmark::kMillisecond);
 
 // First argument is the size of the data, second argument is the size of the
 // alphabet.
-BENCHMARK(BM_Access<uint8_t>)
+BENCHMARK(BM_Rank<uint8_t>)
     ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
                     1'500'000'000, 2'000'000'000},
                    {4, 5, 24, 64, 100, 155, 250},
@@ -203,7 +226,7 @@ BENCHMARK(BM_Access<uint8_t>)
     ->Iterations(5)
     ->Unit(benchmark::kMillisecond);
 
-// BENCHMARK(BM_Access<uint16_t>)
+// BENCHMARK(BM_Rank<uint16_t>)
 //     ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
 //                     1'200'000'000},
 //                    {500, 1'000, 2'000, 4'000, 8'000, 16'000, 32'000,

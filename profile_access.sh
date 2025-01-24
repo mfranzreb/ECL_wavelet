9f9b24f639e44198f8575d826b858513b66fd21d
Marco Franzreb <franzrebmarco@gmail.com>
2025-01-24 17:10:18 +0100

    Small fix

--- profile_access.sh
+++ profile_access.sh
@@ -3,6 +3,13 @@
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_BENCHMARKS=ON -DPROFILE=ON -S . -B ./build
 
 cmake --build ./build --target ecl_access_profile
+
+ncu --export "./access_1G_4_500000" --force-overwrite --kernel-id ::accessKernel: --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --import-source yes ./build/benchmark/ecl_access_profile 1000000000 4 500000
+
+ncu --export "./access_1G_150_500000" --force-overwrite --kernel-id ::accessKernel: --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --import-source yes ./build/benchmark/ecl_access_profile 1000000000 150 500000
+
+ncu --export "./access_500M_20000_500000" --force-overwrite --kernel-id ::accessKernel: --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --import-source yes ./build/benchmark/ecl_access_profile 500000000 20000 500000
+
 nvprof --profile-from-start off --export-profile "./access_1G_4_500000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 4 500000
 
 nvprof --profile-from-start off --export-profile "./access_1G_150_500000_vprof.prof" -f --trace api,gpu ./build/benchmark/ecl_access_profile 1000000000 150 500000

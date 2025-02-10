518862cb73c9463cecd7a8f808068dc17a251f5e
Marco Franzreb <franzrebmarco@gmail.com>
2025-02-10 15:40:02 +0100

    Add all TPQ to select profile

--- benchmark/profile_select.cu
+++ benchmark/profile_select.cu
@@ -7,7 +7,7 @@
 
 template <typename T>
 void profileSelect(size_t const data_size, size_t const alphabet_size,
-                   size_t const num_queries) {
+                   size_t const num_queries, bool const use_profiler_api) {
   std::vector<T> alphabet;
   alphabet = std::vector<T>(alphabet_size);
   std::iota(alphabet.begin(), alphabet.end(),
@@ -18,9 +18,24 @@ void profileSelect(size_t const data_size, size_t const alphabet_size,
       ecl::generateRandomSelectQueries<T>(hist, num_queries, alphabet);
   ecl::WaveletTree<T> wt(data.data(), data_size, std::move(alphabet), 0);
 
-  cudaProfilerStart();
-  auto results = wt.select(queries);
-  cudaProfilerStop();
+  auto queries_copy = queries;
+  auto results = wt.template select<1>(queries_copy);
+  queries_copy = queries;
+  if (use_profiler_api) {
+    cudaProfilerStart();
+    results = wt.template select<1>(queries_copy);
+    cudaProfilerStop();
+    queries_copy = queries;
+  }
+  results = wt.template select<2>(queries_copy);
+  queries_copy = queries;
+  results = wt.template select<4>(queries_copy);
+  queries_copy = queries;
+  results = wt.template select<8>(queries_copy);
+  queries_copy = queries;
+  results = wt.template select<16>(queries_copy);
+  queries_copy = queries;
+  results = wt.template select<32>(queries_copy);
 }
 
 int main(int argc, char** argv) {
@@ -28,10 +43,13 @@ int main(int argc, char** argv) {
   auto const data_size = std::stoul(argv[1]);
   auto const alphabet_size = std::stoul(argv[2]);
   auto const num_queries = std::stoul(argv[3]);
+  bool const use_profiler_api = argc > 4 ? std::stoi(argv[4]) : false;
 
   if (alphabet_size < std::numeric_limits<uint8_t>::max()) {
-    profileSelect<uint8_t>(data_size, alphabet_size, num_queries);
+    profileSelect<uint8_t>(data_size, alphabet_size, num_queries,
+                           use_profiler_api);
   } else if (alphabet_size < std::numeric_limits<uint16_t>::max()) {
-    profileSelect<uint16_t>(data_size, alphabet_size, num_queries);
+    profileSelect<uint16_t>(data_size, alphabet_size, num_queries,
+                            use_profiler_api);
   }
 }
--- profile_select.sh
+++ profile_select.sh
@@ -10,8 +10,8 @@ ncu --export "./select_1G_150_500000" --force-overwrite --kernel-id ::selectKern
 
 ncu --export "./select_500M_20000_500000" --force-overwrite --kernel-id ::selectKernel: --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --import-source yes ./build/benchmark/ecl_select_profile 500000000 20000 500000
 
-nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_1G_4_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 1000000000 4 500000
+nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_1G_4_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 1000000000 4 500000 1
 
-nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_1G_150_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 1000000000 150 500000
+nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_1G_150_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 1000000000 150 500000 1
 
-nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_500M_20000_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 500000000 20000 500000
+nsys profile --cuda-graph-trace=node --capture-range=cudaProfilerApi --output="./select_500M_20000_500000" --force-overwrite=true --trace=cuda,osrt ./build/benchmark/ecl_select_profile 500000000 20000 500000 1

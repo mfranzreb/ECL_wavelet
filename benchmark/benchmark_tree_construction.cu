f2b3c5de2e3a71dd2e874ef623fc20c708bb2db2
Marco Franzreb <franzrebmarco@gmail.com>
2025-03-04 16:24:28 +0100

    Improve benchmarks

--- benchmark/benchmark_tree_construction.cu
+++ benchmark/benchmark_tree_construction.cu
@@ -1,7 +1,4 @@
 #include <benchmark/benchmark.h>
-#include <nvbio/basic/packed_vector.h>
-#include <nvbio/strings/alphabet.h>
-#include <nvbio/strings/wavelet_tree.h>
 
 #include <random>
 #include <utils.cuh>
@@ -16,6 +13,7 @@ static void BM_WaveletTreeConstruction(benchmark::State& state) {
   auto const data_size = state.range(0);
   auto const alphabet_size = state.range(1);
   bool const is_min_alphabet = state.range(2);
+  bool const use_alphabet = state.range(3);
 
   std::vector<T> alphabet;
   std::vector<T> data;
@@ -43,158 +41,25 @@ static void BM_WaveletTreeConstruction(benchmark::State& state) {
       std::this_thread::yield();
     }
     auto alphabet_copy = alphabet;
-    auto wt =
-        WaveletTree<T>(data.data(), data_size, std::move(alphabet_copy), 0);
+    auto wt = WaveletTree<T>(
+        data.data(), data_size,
+        use_alphabet ? std::move(alphabet_copy) : std::vector<T>(), 0);
     done = true;
     t.join();
     state.counters["memory_usage"] = max_memory_usage;
   }
 
-  for (auto _ : state) {
-    state.PauseTiming();
-    auto alphabet_copy = alphabet;
-    state.ResumeTiming();
-    WaveletTree<T> wt(data.data(), data_size, std::move(alphabet_copy), 0);
-  }
-}
-
-template <nvbio::Alphabet AlphabetType>
-nvbio::PackedVector<nvbio::device_tag,
-                    nvbio::AlphabetTraits<AlphabetType>::SYMBOL_SIZE, true>
-getNVbioArgs(size_t const data_size, uint8_t const alphabet_size = 0) {
-  std::vector<uint8_t> alphabet;
-  std::vector<uint8_t> data(data_size);
-  if constexpr (AlphabetType == nvbio::DNA) {
-    alphabet = std::vector<uint8_t>{'A', 'C', 'G', 'T'};
-    data = generateRandomData<uint8_t>(alphabet, data_size);
-  } else if constexpr (AlphabetType == nvbio::DNA_N) {
-    alphabet = std::vector<uint8_t>{'A', 'C', 'G', 'T', 'N'};
-    data = generateRandomData<uint8_t>(alphabet, data_size);
-  } else if constexpr (AlphabetType == nvbio::PROTEIN) {
-    alphabet = std::vector<uint8_t>{'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                                    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                                    'S', 'T', 'V', 'W', 'Y', 'B', 'Z', 'X'};
-    data = generateRandomData<uint8_t>(alphabet, data_size);
-  } else if constexpr (AlphabetType == nvbio::ASCII) {
-    std::tie(alphabet, data) =
-        generateRandomAlphabetAndData<uint8_t>(alphabet_size, data_size, true);
-  }
-  uint32_t const alphabet_bits =
-      nvbio::AlphabetTraits<AlphabetType>::SYMBOL_SIZE;
-
-  // allocate a host packed vector
-  nvbio::PackedVector<nvbio::host_tag, alphabet_bits, true> h_data(data_size);
-
-  // pack the string
-  nvbio::from_string<AlphabetType>(
-      reinterpret_cast<const char*>(data.data()),
-      reinterpret_cast<const char*>(data.data() + data.size()), h_data.begin());
-
-  // copy it to the device
-  nvbio::PackedVector<nvbio::device_tag, alphabet_bits, true> d_data(h_data);
-
-  return d_data;
-}
-
-static void BM_NVBIO(benchmark::State& state) {
-  if (state.range(0) > std::numeric_limits<uint32_t>::max()) {
-    state.SkipWithError("Data size is too large for NVBIO.");
-    return;
-  }
-  uint32_t const data_size = static_cast<uint32_t>(state.range(0));
-  uint8_t const alphabet_size = static_cast<uint8_t>(state.range(1));
-
-  state.counters["param.data_size"] = data_size;
-  state.counters["param.alphabet_size"] = alphabet_size;
-
-  if (alphabet_size == 4) {
-    auto d_data = getNVbioArgs<nvbio::DNA>(data_size);
-    nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
-    // Memory usage
-    {
-      size_t max_memory_usage = 0;
-      std::atomic_bool done{false};
-      std::atomic_bool can_start{false};
-      std::thread t(measureMemoryUsage, std::ref(done), std::ref(can_start),
-                    std::ref(max_memory_usage));
-      while (not can_start) {
-        std::this_thread::yield();
-      }
-      nvbio::WaveletTreeStorage<nvbio::device_tag> wt_mem;
-      nvbio::setup(data_size, d_data.begin(), wt_mem);
-      done = true;
-      t.join();
-      state.counters["memory_usage"] = max_memory_usage;
-    }
+  try {
     for (auto _ : state) {
-      nvbio::setup(data_size, d_data.begin(), wt);
+      state.PauseTiming();
+      auto alphabet_copy = alphabet;
+      state.ResumeTiming();
+      WaveletTree<T> wt(
+          data.data(), data_size,
+          use_alphabet ? std::move(alphabet_copy) : std::vector<T>(), 0);
     }
-  } else if (alphabet_size == 5) {
-    auto d_data = getNVbioArgs<nvbio::DNA_N>(data_size);
-    nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
-    // Memory usage
-    {
-      size_t max_memory_usage = 0;
-      std::atomic_bool done{false};
-      std::atomic_bool can_start{false};
-      std::thread t(measureMemoryUsage, std::ref(done), std::ref(can_start),
-                    std::ref(max_memory_usage));
-      while (not can_start) {
-        std::this_thread::yield();
-      }
-      nvbio::WaveletTreeStorage<nvbio::device_tag> wt_mem;
-      nvbio::setup(data_size, d_data.begin(), wt_mem);
-      done = true;
-      t.join();
-      state.counters["memory_usage"] = max_memory_usage;
-    }
-    for (auto _ : state) {
-      nvbio::setup(data_size, d_data.begin(), wt);
-    }
-  } else if (alphabet_size == 24) {
-    auto d_data = getNVbioArgs<nvbio::PROTEIN>(data_size);
-    nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
-    // Memory usage
-    {
-      size_t max_memory_usage = 0;
-      std::atomic_bool done{false};
-      std::atomic_bool can_start{false};
-      std::thread t(measureMemoryUsage, std::ref(done), std::ref(can_start),
-                    std::ref(max_memory_usage));
-      while (not can_start) {
-        std::this_thread::yield();
-      }
-      nvbio::WaveletTreeStorage<nvbio::device_tag> wt_mem;
-      nvbio::setup(data_size, d_data.begin(), wt_mem);
-      done = true;
-      t.join();
-      state.counters["memory_usage"] = max_memory_usage;
-    }
-    for (auto _ : state) {
-      nvbio::setup(data_size, d_data.begin(), wt);
-    }
-  } else {
-    auto d_data = getNVbioArgs<nvbio::ASCII>(data_size, alphabet_size);
-    nvbio::WaveletTreeStorage<nvbio::device_tag> wt;
-    // Memory usage
-    {
-      size_t max_memory_usage = 0;
-      std::atomic_bool done{false};
-      std::atomic_bool can_start{false};
-      std::thread t(measureMemoryUsage, std::ref(done), std::ref(can_start),
-                    std::ref(max_memory_usage));
-      while (not can_start) {
-        std::this_thread::yield();
-      }
-      nvbio::WaveletTreeStorage<nvbio::device_tag> wt_mem;
-      nvbio::setup(data_size, d_data.begin(), wt_mem);
-      done = true;
-      t.join();
-      state.counters["memory_usage"] = max_memory_usage;
-    }
-    for (auto _ : state) {
-      nvbio::setup(data_size, d_data.begin(), wt);
-    }
+  } catch (std::runtime_error& e) {
+    state.SkipWithError(e.what());
   }
 }
 
@@ -215,18 +80,12 @@ BENCHMARK(BM_WaveletTreeConstruction<uint8_t>)
     ->Iterations(5)
     ->Unit(benchmark::kMillisecond);
 
-// BENCHMARK(BM_WaveletTreeConstruction<uint16_t>)
-//     ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
-//                     1'200'000'000},
-//                    {500, 1'000, 2'000, 4'000, 8'000, 16'000, 32'000, 64'000},
-//                    {0, 1}})
-//     ->Iterations(5)
-//     ->Unit(benchmark::kMillisecond);
-
-BENCHMARK(BM_NVBIO)
+BENCHMARK(BM_WaveletTreeConstruction<uint16_t>)
     ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
-                    1'500'000'000, 2'000'000'000},
-                   {4, 5, 24, 64, 100, 155, 256}})
+                    1'200'000'000},
+                   {500, 1'000, 2'000, 4'000, 8'000, 16'000, 32'000, 64'000},
+                   {0, 1}})
     ->Iterations(5)
     ->Unit(benchmark::kMillisecond);
+
 }  // namespace ecl
---
+++ benchmark/benchmark_tree_construction_SDSL.cu
@@ -0,0 +1,79 @@
+#include <benchmark/benchmark.h>
+
+#include <random>
+#include <sdsl/int_vector.hpp>
+#include <sdsl/wavelet_trees.hpp>
+#include <utils.cuh>
+
+#include "test_benchmark_utils.cuh"
+
+namespace ecl {
+
+template <typename T>
+static void BM_WaveletTreeConstruction(benchmark::State& state) {
+  auto const data_size = state.range(0);
+  auto const alphabet_size = state.range(1);
+  bool const is_min_alphabet = state.range(2);
+
+  std::vector<T> alphabet;
+  std::vector<T> data;
+  if (is_min_alphabet) {
+    alphabet = std::vector<T>(alphabet_size);
+    std::iota(alphabet.begin(), alphabet.end(), 0);
+    data = generateRandomData<T>(alphabet, data_size);
+  } else {
+    std::tie(alphabet, data) =
+        generateRandomAlphabetAndData<T>(alphabet_size, data_size, true);
+  }
+
+  state.counters["param.data_size"] = data_size;
+  state.counters["param.alphabet_size"] = alphabet_size;
+  state.counters["param.is_min_alphabet"] = is_min_alphabet;
+
+  if constexpr (sizeof(T) == 1) {
+    sdsl::int_vector<8> signed_data(data.size());
+#pragma omp parallel for
+    for (size_t i = 0; i < data.size(); i++) {
+      signed_data.set_int(8 * i, data[i], 8);
+    }
+    for (auto _ : state) {
+      sdsl::wt_pc<sdsl::balanced_shape, sdsl::bit_vector,
+                  sdsl::rank_support_v5<>>
+          wt;
+      sdsl::construct_im(wt, signed_data);
+    }
+  } else {
+    // Change vec to uint64_t
+    sdsl::int_vector<> data64(data.size());
+#pragma omp parallel for
+    for (size_t i = 0; i < data.size(); i++) {
+      data64.set_int(64 * i, static_cast<uint64_t>(data[i]), 64);
+    }
+    for (auto _ : state) {
+      sdsl::wt_pc<
+          sdsl::balanced_shape, sdsl::bit_vector, sdsl::rank_support_v5<>,
+          sdsl::wt_pc<sdsl::balanced_shape>::select_1_type,
+          sdsl::wt_pc<sdsl::balanced_shape>::select_0_type, sdsl::int_tree<>>
+          wt;
+      sdsl::construct_im(wt, data64);
+    }
+  }
+}
+
+BENCHMARK(BM_WaveletTreeConstruction<uint8_t>)
+    ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
+                    1'200'000'000},
+                   {4, 5, 16, 17, 32, 33, 64, 65, 128, 129, 255, 256},
+                   {0, 1}})
+    ->Iterations(5)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK(BM_WaveletTreeConstruction<uint16_t>)
+    ->ArgsProduct({{200'000'000, 500'000'000, 800'000'000, 1'000'000'000,
+                    1'200'000'000},
+                   {300, 1'000, 2'000, 4'000, 8'000, 16'000, 32'000, 64'000},
+                   {0, 1}})
+    ->Iterations(5)
+    ->Unit(benchmark::kMillisecond);
+
+}  // namespace ecl
